97d3791d1505b6429be032e1cad58b4a5016e02b
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2014-02-08 00:55:30 +0900

    commit from 2.6.0-beta4

---
+++ class/Ethna/SOAP/ActionForm.php
@@ -0,0 +1,47 @@
+<?php
+// vim: foldmethod=marker
+/**
+ *  ActionForm.php
+ *
+ *  @author     Masaki Fujimoto <fujimoto@php.net>
+ *  @license    http://www.opensource.org/licenses/bsd-license.php The BSD License
+ *  @package    Ethna
+ *  @version    $Id: 5deb833163f8ae2e4c4b20bd73548a40b5ed1c84 $
+ */
+
+// {{{ Ethna_SOAP_ActionForm
+/**
+ *  SOAPフォームクラス
+ *
+ *  @author     Masaki Fujimoto <fujimoto@php.net>
+ *  @access     public
+ *  @package    Ethna
+ */
+class Ethna_SOAP_ActionForm extends Ethna_ActionForm
+{
+    /**#@+
+     *  @access private
+     */
+
+    /** @var    array   引数定義 */
+    public $arg = array();
+
+    /** @var    array   戻り値定義 */
+    public $retval = array();
+
+    /**#@-*/
+
+    /**
+     *  Ethna_SOAP_ActionFormクラスのコンストラクタ
+     *
+     *  @access public
+     *  @param  object  Ethna_ActionError   $action_error   アクションエラーオブジェクト
+     */
+    public function __construct($action_error)
+    {
+        $this->form = $this->arg;
+
+        parent::__construct($action_error);
+    }
+}
+// }}}
